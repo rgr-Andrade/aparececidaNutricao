d6bdaa52937aacd35a10e7e1dd5962d340a98255
Rogério Andrade <rgrandrade@hotmail.com>
2022-10-07 21:18:06 -0300

    validação e estilos em 'imc' invalidos

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,46 @@
-console.log("Fui carregado arquivo externo")
+var pacientes = document.querySelectorAll(".paciente");
+//console.log(pacientes);
 
-var titulo = document.querySelector(".secao")
 
-titulo.textContent = "Aparecida Nutricionista"
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!"
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválido");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválido!"
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
+}
+
+
+
+
